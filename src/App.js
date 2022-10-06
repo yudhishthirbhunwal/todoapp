5bd94d06a42732f85cad3ef23581c79fae143c02
Yudhishthir <yudhishthir.it21@gmail.com>
2022-10-06 17:59:38 +0530

    [Add task with Enter]:  -Add tasks with enter button now.

--- src/App.js
+++ src/App.js
@@ -6,13 +6,22 @@ function App() {
   const [todos, setTodos] = useState(["Write a task.", "Click on Add button to add it to the list."])
 
   function createTodo() {
-    return setTodos(todos => [...todos, task])
+    return setTodos(todos => {
+      setTask("")
+      return [...todos, task]
+    })
+  }
+
+  function createOnEnter(e) {
+    if (e.keyCode === 13) {
+      createTodo()
+    }
   }
 
   return (
     <div className="App">
       <h1>Add Todo List</h1>
-        <input type="text" value={task} onChange={e => {
+        <input type="text" value={task} onKeyDown={createOnEnter} onChange={e => {
           setTask(e.target.value)
         }} />
         <button onClick={createTodo}>Add</button>
